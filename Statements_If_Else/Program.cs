5500c5e8a83aba8fddf3f07b6892c37d17b5e480
Eber27 <eberfreire@gmail.com>
2021-02-28 21:56:28 -0600

    if_else_Practice

---
+++ Statements_If_Else/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Statements_If_Else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favoritenumber = 5;
+            Console.WriteLine("Which is your favorite Number? Hint: Choose from 1 to 100");
+            int userguess = int.Parse(Console.ReadLine());
+
+            if (favoritenumber > userguess)
+            {
+                Console.WriteLine("Too Low");
+            }
+
+            else if (favoritenumber < userguess)
+            {
+                Console.WriteLine("Too High");
+            }
+
+            else if (favoritenumber == userguess)
+
+            {
+                Console.WriteLine("You`re right!");
+            }
+
+            else
+
+            {
+                Console.WriteLine("NeverMind");
+            }
+
+
+
+        }
+    }
+}
